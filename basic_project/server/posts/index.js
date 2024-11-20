e3c5bd6387f1009ee5ffccda6a251a783dcf4771
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-20 09:16:38 +0530

    feat: created simple post service

---
+++ basic_project/server/posts/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { randomBytes } from "crypto";
+
+const app = express();
+
+app.use(express.json());
+
+const posts = {};
+
+app.get(`/posts`, (req, res) => {
+    res.send(posts);
+});
+
+app.post(`/posts`, (req, res) => {
+    const { title } = req.body;
+    if (!title) {
+        return res.status(400).json({ message: "title is required." });
+    }
+    const id = randomBytes(4).toString('hex');
+    posts[id] = {
+        id,
+        title
+    };
+    res.status(201).json({ id });
+});
+
+app.listen(8001, () => {
+    console.log(`Posts service listening on port 8001`)
+});
