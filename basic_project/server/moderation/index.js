d6030b0e01dcb9b207e9db420e6b1442fdd0663a
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-20 19:40:19 +0530

    feat: completed comment moderation service

---
+++ basic_project/server/moderation/index.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get(`/`, (req, res) => {
+    res.json({ });
+});
+
+app.post(`/events`, async (req, res) => {
+    const { type, data } = req.body;
+    switch (type) {
+        case "COMMENT_CREATED": {
+            const prohibitedWords = ["stupid", "idiot", "dumb", "fool", "nonsense", "moron"];
+            const content = data?.content?.toLowerCase(); 
+            const status = prohibitedWords.some(word => content?.includes(word))
+                ? "rejected"
+                : "approved";
+            await axios.post(`http://localhost:8080/events`, {
+                type: "COMMENT_MODERATED",
+                data: {
+                    ...data,
+                    status
+                }
+            });
+            break;
+        }
+
+        default: {
+            console.log(`Unhandled event type: ${type}`);
+            break;
+        }
+    }
+    res.json({ status: 'OK' });
+});
+
+app.listen(8004, () => {
+    console.log(`Query service listening on port 8004`)
+});
