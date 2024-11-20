6fc8c7a85a4512d7722e771964ce4a3237371c75
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-20 14:11:28 +0530

    feat: added cors

--- basic_project/server/comments/index.js
+++ basic_project/server/comments/index.js
@@ -1,8 +1,10 @@
+import cors from "cors";
 import express from "express";
 import { randomBytes } from "crypto";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const commentsByPostId = {};
