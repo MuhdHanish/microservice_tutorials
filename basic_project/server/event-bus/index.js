0a4f02cb84d0c107c2ad114177e96a44e0e63968
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-20 19:44:06 +0530

    feat: added comments for identify which service and added extra comment moderation service

--- basic_project/server/event-bus/index.js
+++ basic_project/server/event-bus/index.js
@@ -8,9 +8,10 @@ app.use(express.json());
 app.post(`/events`, (req, res) => {
     const event = req.body;
 
-    axios.post(`http://localhost:8001/events`, event);
-    axios.post(`http://localhost:8002/events`, event);
-    axios.post(`http://localhost:8003/events`, event);
+    axios.post(`http://localhost:8001/events`, event); // POST
+    axios.post(`http://localhost:8002/events`, event); // COMMENT
+    axios.post(`http://localhost:8003/events`, event); // QUERY
+    axios.post(`http://localhost:8004/events`, event); // COMMENT-MODERATION
 
     res.json({ status: 'OK' });
 });
