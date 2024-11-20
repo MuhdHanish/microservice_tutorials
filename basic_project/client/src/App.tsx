bacf8f855b3d457107ef36ed4a4a5eeba66dbbda
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-20 14:30:39 +0530

    refactor: added the class for the parent

--- basic_project/client/src/App.tsx
+++ basic_project/client/src/App.tsx
@@ -2,9 +2,9 @@ import { Posts } from "./components"
 
 function App() {
   return (
-    <>
+    <div className="container" style={{ marginTop: 10 }}>
       <Posts />
-    </>
+    </div>
   )
 }
 
