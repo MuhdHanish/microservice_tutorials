7c482ad7bf0f89463aeb355ca11610110b941998
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-20 14:30:21 +0530

    feat: added comments in posts

--- basic_project/client/src/components/posts.tsx
+++ basic_project/client/src/components/posts.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FormEvent, useCallback, useEffect, useState } from "react"
+import { Comments } from "./comments";
 
 export const Posts = () => {
     const [loading, setLoading] = useState(false);
@@ -36,30 +37,29 @@ export const Posts = () => {
     }, []);
     return (
         <>
-            <div className="container" style={{ marginTop: 10 }}>
-                <h1>Create Post</h1>
-                <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: 15 }}>
-                    <div className="form-group" >
-                        <label htmlFor="title">Title</label>
-                        <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" className="form-control" id="title" placeholder="Enter title" />
-                    </div>
-                    <button disabled={loading} type="submit" className="btn btn-primary">
-                        {loading ? "Submittng..." : "Submit"}
-                    </button>
-                </form>
-                <hr />
-                <h1 style={{ marginBottom: 10 }}>Posts</h1>
-                <div className="d-flex flex-row flex-wrap gap-4">
-                    {
-                        posts?.map((post, index) => (
-                            <div className="card" style={{ width: '30%', marginBottom: "20px" }} key={`${post?.id}-${index}`}>
-                                <div className="card-body">
-                                    <h3>{post?.title}</h3>
-                                </div>
-                            </div>
-                        ))
-                    }
+            <h1>Create Post</h1>
+            <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: 15 }}>
+                <div className="form-group" >
+                    <label htmlFor="title">Title</label>
+                    <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" className="form-control" id="title" placeholder="Enter title" />
                 </div>
+                <button disabled={loading} type="submit" className="btn btn-primary">
+                    {loading ? "Submittng..." : "Submit"}
+                </button>
+            </form>
+            <hr />
+            <h1 style={{ marginBottom: 10 }}>Posts</h1>
+            <div className="d-flex flex-row flex-wrap gap-4">
+                {
+                    posts?.map((post, index) => (
+                        <div className="card" style={{ width: '30%', marginBottom: "20px" }} key={`${post?.id}-${index}`}>
+                            <div className="card-body">
+                                <h3>{post?.title}</h3>
+                                <Comments postId={post?.id} />
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
