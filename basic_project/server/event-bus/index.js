f15f462b1fa31618208746c21a5ca8a5a0ba82a2
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-20 22:42:21 +0530

    refactor: updated event-but and added the events

--- basic_project/server/event-bus/index.js
+++ basic_project/server/event-bus/index.js
@@ -5,16 +5,41 @@ const app = express();
 
 app.use(express.json());
 
-app.post(`/events`, (req, res) => {
+const events = []; 
+
+const services = [
+    { name: "POST", url: "http://localhost:8001/events" },
+    { name: "COMMENT", url: "http://localhost:8002/events" },
+    { name: "QUERY", url: "http://localhost:8003/events" },
+    { name: "COMMENT_MODERATION", url: "http://localhost:8004/events" },
+];
+
+app.post(`/events`, async (req, res) => {
     const event = req.body;
 
-    axios.post(`http://localhost:8001/events`, event); // POST
-    axios.post(`http://localhost:8002/events`, event); // COMMENT
-    axios.post(`http://localhost:8003/events`, event); // QUERY
-    axios.post(`http://localhost:8004/events`, event); // COMMENT-MODERATION
+    events.push(event);
+    try {
+        await Promise.all(
+            services.map(async (service) => {
+                try {
+                    await axios.post(service.url, event);
+                    console.log(`[INFO] Event sent to ${service.name}`);
+                } catch (error) {
+                    console.error(`[ERROR] Failed to send event to ${service.name}: ${error}`);
+                }
+            })
+        );
 
-    res.json({ status: 'OK' });
+        res.json({ status: "OK" });
+    } catch (error) {
+        console.error(`[ERROR] Unexpected error: ${error.message}`);
+        res.status(500).json({ status: "ERROR", message: "Failed to propagate events" });
+    }
 });
+
+app.get(`/events`, (req, res) => {
+    res.json({ events });
+})
 
 app.listen(8080, () => {
     console.log(`Event but listing on port 8080`);
