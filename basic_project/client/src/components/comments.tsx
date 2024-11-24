fe7adc09dc2d982da8305bcdea4b80f9012af0eb
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-24 09:32:32 +0530

    refactor: changed posts.com to node-microservices-tutorial-basic-project.com

--- basic_project/client/src/components/comments.tsx
+++ basic_project/client/src/components/comments.tsx
@@ -16,7 +16,7 @@ export const Comments = ({
         if (!comment) return;
         try {
             setLoading(true);
-            await axios.post(`http://posts.com/posts/${postId}/comments/create`, {
+            await axios.post(`http://node-microservices-tutorial-basic-project.com/posts/${postId}/comments/create`, {
                 content: comment
             });
             fetchComments();
@@ -30,7 +30,7 @@ export const Comments = ({
 
     const fetchComments = useCallback(async () => {
         try {
-            const response = await axios.get(`http://posts.com/posts/${postId}/comments`);
+            const response = await axios.get(`http://node-microservices-tutorial-basic-project.com/posts/${postId}/comments`);
             setComments(Object.values(response?.data?.comments) || []);
         } catch (error) {
             console.error(error);
