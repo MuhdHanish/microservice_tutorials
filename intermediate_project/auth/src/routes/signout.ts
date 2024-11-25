b97e2a783e21b1910ebf706e8898a34a5a3915da
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-25 23:07:03 +0530

    feat: completed signout

--- intermediate_project/auth/src/routes/signout.ts
+++ intermediate_project/auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import { Request, Response, Router } from "express";
 const router = Router();
 
 router.post("/signout", (req: Request, res: Response) => {
-    res.send({});
+    req.session = null;
+    res.status(200).send({ message: "Signed out successfully." });
 });
 
 export { router as signoutRouter };
