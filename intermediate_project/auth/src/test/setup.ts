6c56a32bf19d830b29f3c605b4d13ea5b796b3ac
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-26 00:17:53 +0530

    chore: created test setup.ts file

---
+++ intermediate_project/auth/src/test/setup.ts
@@ -0,0 +1,24 @@
+import { config } from "dotenv";
+import path from "path";
+
+config({ path: path.resolve(__dirname, "../../.env.test.local") });
+
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const URI = mongo.getUri();
+    await mongoose.connect(URI);
+});
+
+afterAll(async () => {
+    const collections = await mongoose.connection.db?.collections() || [];
+    for (const collection of collections) {
+        await collection.deleteMany({});
+    }
+    if (mongo) await mongo.stop();
+    await mongoose.connection.close();
+});
