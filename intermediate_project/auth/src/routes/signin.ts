b1b3a94d441a8a149d277dbb136b311c6d64d9d7
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-25 17:02:07 +0530

    feat: added validation-middleware

--- intermediate_project/auth/src/routes/signin.ts
+++ intermediate_project/auth/src/routes/signin.ts
@@ -1,9 +1,29 @@
 import { Request, Response, Router } from "express";
+import { CustomHTTPError } from "../lib/utils";
+import { validationMiddleware } from "../middlewares";
+import { validateEmailPassword } from "../lib/validations";
 
 const router = Router();
 
-router.post("/signin", (req: Request, res: Response) => {
-    res.send({});
-});
+router.post("/signin",
+    validationMiddleware(validateEmailPassword),
+    (req: Request, res: Response) => {
+        try {
+            const { email, password } = req.body;
+            if (!email || !password) {
+                throw new CustomHTTPError("Email and password are required.", 400);
+            }
+            res.send({});
+        } catch (error: any) {
+            if (error instanceof CustomHTTPError) {
+                res.status(error.status).send({ message: error?.message });
+            } else {
+                res.status(500).send({
+                    message: "Something went wrong.",
+                    error: error?.message || "Unexpected error."
+                });
+            }
+        }
+    });
 
 export { router as signinRouter };
