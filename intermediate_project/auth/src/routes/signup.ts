025ebe287e8f69ba6a9cc968ee659ffb0ebda7b1
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-25 20:41:14 +0530

    feat: added user creation in signup

--- intermediate_project/auth/src/routes/signup.ts
+++ intermediate_project/auth/src/routes/signup.ts
@@ -2,18 +2,28 @@ import { NextFunction, Request, Response, Router } from "express";
 import { CustomHTTPError } from "../lib/utils";
 import { validationHandler } from "../middlewares";
 import { validateEmailPassword } from "../lib/validations";
+import { User } from "../models";
 
 const router = Router();
 
 router.post("/signup",
     validationHandler(validateEmailPassword),
-    (req: Request, res: Response, next: NextFunction) => {
+    async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { email, password } = req.body;
         if (!email || !password) {   
             throw new CustomHTTPError("Email and password are required.", 400);
         }
-        res.send({});
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            throw new CustomHTTPError("Email already exists.", 409);
+        }
+
+        const user = User.build({ email, password });
+        await user.save();
+
+        res.status(201).send({ user });
     } catch (error: any) {
         next(error);
     }
