f5dfc0ff2dbe4466eeb9b1e23c8599faad4d41ac
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-25 17:38:05 +0530

    feat: added not found handler + global error handler

--- intermediate_project/auth/src/index.ts
+++ intermediate_project/auth/src/index.ts
@@ -1,11 +1,19 @@
 import express from "express";
 import { authRouter } from "./routes";
+import { errorHandler } from "./middlewares";
+import { CustomHTTPError } from "./lib/utils";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
+
+app.use("*", (req, res, next) => {
+    errorHandler(new CustomHTTPError("Resource not found.", 404), req, res, next);
+});
+
+app.use(errorHandler);
 
 app.listen(8001, () => {
     console.log("Auth service listening on port 8001");
