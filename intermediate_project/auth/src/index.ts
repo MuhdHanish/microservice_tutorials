7b2e6cfa78c6ff887c24066ceb94dbb33ac35661
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-25 15:43:54 +0530

    feat: created basic setup for auth service

---
+++ intermediate_project/auth/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello from auth service");
+});
+
+app.listen(8001, () => {
+    console.log("Auth service listening on port 8001");
+});
