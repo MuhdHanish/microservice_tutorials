33c9725a0036cb455a81971603cdecf2c0fc4aa8
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-25 23:23:34 +0530

    refactor: added auth handler in /currentuser

--- intermediate_project/auth/src/routes/current-user.ts
+++ intermediate_project/auth/src/routes/current-user.ts
@@ -1,23 +1,16 @@
-import jwt from "jsonwebtoken";
-import { CustomHTTPError } from "../lib/utils";
+import { authHandler } from "../middlewares";
 import { NextFunction, Request, Response, Router } from "express";
 
 const router = Router();
 
-router.get("/currentuser", (req: Request, res: Response, next: NextFunction) => {
-    try {
-        if (!req?.session?.token) { 
-            throw new CustomHTTPError("No authorization token found.", 401);
+router.get("/currentuser",
+    authHandler,
+    (req: Request, res: Response, next: NextFunction) => {
+        try {
+            res.send({ user: req?.user || null });
+        } catch (error) {
+            next(error);
         }
-        const { token } = req?.session;
-        const payload = jwt.verify(token, process.env.JWT_SECRET!);
-        res.send({ user: payload });
-    } catch (error) {
-        if (error instanceof jwt.JsonWebTokenError) {
-            return next(new CustomHTTPError("Invalid token, Access forbidden.", 403));
-        }
-        next(error);
-    }
-});
+    });
 
 export { router as currentUserRouter };
