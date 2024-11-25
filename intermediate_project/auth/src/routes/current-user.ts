6be990e5ef74eb10120fcf9cded1590b7e324000
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-25 23:03:07 +0530

    feat: completed the get currentuser route handling token validations

--- intermediate_project/auth/src/routes/current-user.ts
+++ intermediate_project/auth/src/routes/current-user.ts
@@ -1,9 +1,23 @@
-import { Request, Response, Router } from "express";
+import jwt from "jsonwebtoken";
+import { CustomHTTPError } from "../lib/utils";
+import { NextFunction, Request, Response, Router } from "express";
 
 const router = Router();
 
-router.get("/currentuser", (req: Request, res: Response) => {
-    res.send({ user: "Muhammed Hanish" });
+router.get("/currentuser", (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (!req?.session?.token) { 
+            throw new CustomHTTPError("No authorization token found.", 401);
+        }
+        const { token } = req?.session;
+        const payload = jwt.verify(token, process.env.JWT_SECRET!);
+        res.send({ user: payload });
+    } catch (error) {
+        if (error instanceof jwt.JsonWebTokenError) {
+            return next(new CustomHTTPError("Invalid token, Access forbidden.", 403));
+        }
+        next(error);
+    }
 });
 
 export { router as currentUserRouter };
