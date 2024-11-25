72974dcf8bae3713009b23aecd26869123ff749e
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-25 20:49:58 +0530

    feat: added login credentials checking

--- intermediate_project/auth/src/routes/signin.ts
+++ intermediate_project/auth/src/routes/signin.ts
@@ -1,19 +1,26 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { User } from "../models";
 import { CustomHTTPError } from "../lib/utils";
 import { validationHandler } from "../middlewares";
 import { validateEmailPassword } from "../lib/validations";
+import { NextFunction, Request, Response, Router } from "express";
 
 const router = Router();
 
 router.post("/signin",
     validationHandler(validateEmailPassword),
-    (req: Request, res: Response, next: NextFunction) => {
+    async (req: Request, res: Response, next: NextFunction) => {
         try {
             const { email, password } = req.body;
             if (!email || !password) {
                 throw new CustomHTTPError("Email and password are required.", 400);
             }
-            res.send({});
+
+            const user = await User.findOne({ email });
+            if (!user || !await user.comparePassword(password)) {
+                throw new CustomHTTPError("Credentials are not valid.", 401);
+            }
+
+            res.send({ user });
         } catch (error: any) {
             next(error);
         }
