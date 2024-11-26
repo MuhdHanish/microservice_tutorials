4c7f90964345bf4b4f7da6156befed76834f2c90
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-26 15:08:34 +0530

    refactor: error-handler

--- intermediate_project/auth/src/middlewares/error-handler.ts
+++ intermediate_project/auth/src/middlewares/error-handler.ts
@@ -1,4 +1,4 @@
-import e, { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CustomHTTPError } from "../lib/utils";
 
 export const errorHandler = (
