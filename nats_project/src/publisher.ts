ae6421001aef69bde6de0fbdb4cfc8bf64d71b5f
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-27 05:11:34 +0530

    feat: created publisher

---
+++ nats_project/src/publisher.ts
@@ -0,0 +1,31 @@
+import nats from "node-nats-streaming";
+import { randomBytes } from "crypto";
+
+console.clear();
+
+const clientId = randomBytes(4).toString("hex");
+
+// client - stan
+const stan = nats.connect("ticketing", clientId, {
+    url: "http://localhost:4222"
+});
+
+stan.on("connect", () => {
+    console.log(`Publisher ${clientId} connected to NATS`);
+
+    const data = JSON.stringify({
+        id: "123",
+        title: "concert",
+        price: 20
+    });
+
+    stan.publish("ticket:created", data, () => {
+        console.log("Event ticket:created published");
+        stan.close();
+    });
+
+    stan.on("close", () => {
+        console.log("PublisherNATS connection closed!");
+        process.exit();
+    });
+})
