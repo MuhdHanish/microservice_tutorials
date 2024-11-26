11cf9d087207d2697e42b1f3fa67a416d06efd1c
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-26 19:01:24 +0530

    feat: updated app.ts with adding session

--- intermediate_project/tickets/src/app.ts
+++ intermediate_project/tickets/src/app.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import {
+    authHandler,
     CustomHTTPError,
     errorHandler
 } from "@hanishdev-ticketing/common";
+import { tickerRouter } from "./routes";
+import cookieSession from "cookie-session";
 
 const app = express();
 
 app.set("trust proxy", true);
 
 app.use(express.json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+}));
 
-app.use("/api/tickets", (req, res) => {
-    res.status(200);
-});
+
+app.use("/api/tickets", authHandler, tickerRouter);
 
 app.use("*", (req, res, next) => {
     errorHandler(new CustomHTTPError("Resource not found.", 404), req, res, next);
