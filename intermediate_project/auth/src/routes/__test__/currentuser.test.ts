e7e8998ff4f3af1821a7839b33dcef8d43d0a52a
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-26 07:06:19 +0530

    refactor: refactored current user test

--- intermediate_project/auth/src/routes/__test__/currentuser.test.ts
+++ intermediate_project/auth/src/routes/__test__/currentuser.test.ts
@@ -2,31 +2,31 @@ import request from "supertest";
 import { app } from "../../app";
 
 describe("currentuser", () => { 
-    it("returns a 4XX if no token is provided or invalid", async () => { 
+    it("returns a 401 or 403 if no token is provided or invalid", async () => { 
         const response = await request(app)
             .get("/api/auth/currentuser")
             .send({});
         const status = response.status;
-        expect(status?.toString().startsWith('4')).toBe(true);
+        expect([401, 403].includes(status)).toBe(true);
     });
     
     it("return a 200 and user if token is provided and valid", async () => {
-        const response = await request(app)
+        const signupResponse = await request(app)
             .post("/api/auth/signup")
             .send({
                 email: "jhondoe@example.com",
                 password: "Jhone@123",
             })
             .expect(201);
-        const cookie = response.get("Set-Cookie") as string[];
+        const cookie = signupResponse.get("Set-Cookie") as string[];
         expect(cookie).toBeDefined();
-        const response2 = await request(app)
+        const response = await request(app)
             .get("/api/auth/currentuser")
             .set("Cookie", cookie)
             .send({});
-        expect(response2.status).toBe(200);
-        expect(response2.body.user).toBeDefined();
-        expect(response2.body.user).toHaveProperty("id");
-        expect(response2.body.user).toHaveProperty("email");
+        expect(response.status).toBe(200);
+        expect(response.body.user).toBeDefined();
+        expect(response.body.user).toHaveProperty("id");
+        expect(response.body.user).toHaveProperty("email");
     });
 });
