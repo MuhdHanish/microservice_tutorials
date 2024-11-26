dbeada55fb18a8fe3fc6659a5e51c737143b0f6f
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-26 21:32:15 +0530

    feat: compeleted get ticket by id

--- intermediate_project/tickets/src/routes/index.ts
+++ intermediate_project/tickets/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { createTickerRouter } from "./create-ticket";
+import { getTicketByIdRouter } from "./get-ticket-by-id";
 
 const tickerRouter = Router();
 
 tickerRouter.use(createTickerRouter);
+tickerRouter.use(getTicketByIdRouter);
 
 export { tickerRouter };
