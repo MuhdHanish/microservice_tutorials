006495e716ba5fdba5fffdcd53fede920acd5055
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-27 19:08:10 +0530

    feat: completed basic setups of orders in a single go

---
+++ intermediate_project/orders/src/app.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import {
+    authHandler,
+    CustomHTTPError,
+    errorHandler
+} from "@hanishdev-ticketing/common";
+import cookieSession from "cookie-session";
+
+const app = express();
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+}));
+
+app.use("/api/orders", authHandler, (req, res) => {
+    res.send({});
+});
+
+app.use("*", (req, res, next) => {
+    errorHandler(new CustomHTTPError("Resource not found.", 404), req, res, next);
+});
+
+app.use(errorHandler);
+
+export { app };
