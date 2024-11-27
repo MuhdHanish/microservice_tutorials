a433f90737594c8aa261ab1689d1fbaf2a65cb11
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-27 07:51:52 +0530

    refactor: updated publisher servcie returning promise

--- nats_project/src/events/base-publisher.ts
+++ nats_project/src/events/base-publisher.ts
@@ -10,11 +10,17 @@ export abstract class Publisher<T extends Event> {
     abstract subject: T["subject"];
 
     constructor(private client: Stan) { }
-    
-    publish(data: T["data"]) {
-        this.client.publish(this.subject, JSON.stringify(data), () => {
-            console.log(`Event ${this.subject} published`);
-        });
+
+    publish(data: T["data"]): Promise<void> {
+        return new Promise((resolve, reject) => {
+            this.client.publish(this.subject, JSON.stringify(data), (error) => {
+                if (error) {
+                    return reject(error);
+                }
+                console.log(`Event ${this.subject} published`);
+                resolve();
+            });
+        })
     }
 
 }
