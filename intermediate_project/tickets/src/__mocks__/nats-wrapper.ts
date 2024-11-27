c69c67652a8237ded525109639dce35553f51e5c
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-27 18:27:11 +0530

    feat: added natsWrapper client - publish as an jest func to make sure that called

--- intermediate_project/tickets/src/__mocks__/nats-wrapper.ts
+++ intermediate_project/tickets/src/__mocks__/nats-wrapper.ts
@@ -1,7 +1,9 @@
 export const natsWrapper = {
     client: {
-        publish: (subject: string, data: any, callback: () => void) => {
-            callback();
-        }
+        publish: jest.fn().mockImplementation(
+            (subject: string, data: any, callback: () => void) => {
+                callback();
+            }
+        )
     }
 };
