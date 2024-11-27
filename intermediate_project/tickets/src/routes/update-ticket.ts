7a09642d1869cf8cd9523cb9cbcc68edb8288f35
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-27 11:26:27 +0530

    feat: added update-ticket publisher

--- intermediate_project/tickets/src/routes/update-ticket.ts
+++ intermediate_project/tickets/src/routes/update-ticket.ts
@@ -1,9 +1,11 @@
 import { Ticket } from "../models";
 import {
     CustomHTTPError,
+    TicketUpdatedPublisher,
     validationHandler,
 } from "@hanishdev-ticketing/common";
 import { validateUpdateTicket } from "../lib";
+import { natsWrapper } from "../nats-wrapper";
 import { NextFunction, Request, Response, Router } from "express";
 
 const router = Router();
@@ -16,24 +18,25 @@ router.put("/:id",
             if (!id) { 
                 throw new CustomHTTPError("ID is required.", 400);
             }
-
             const { title, price } = req.body;
             if (!title || !price) {
                 throw new CustomHTTPError("Title and price are required.", 400);
             }
-
             const existingTicket = await Ticket.findById(id);
             if (!existingTicket) {
                 throw new CustomHTTPError("Ticket not found with provided ID.", 404);
             }
-
             if (existingTicket.user !== req.user!.id) {
                 throw new CustomHTTPError("User doesn't have access to update this ticket.", 403);
             }
-
             existingTicket.set({ title, price });
             await existingTicket.save();
-
+            new TicketUpdatedPublisher(natsWrapper.client).publish({
+                id: existingTicket.id,
+                title: existingTicket.title,
+                price: existingTicket.price,
+                user: existingTicket.user
+            })
             res.status(200).send({ ticket: existingTicket });
         } catch (error: any) {
             next(error);
