76eca047774960d07344810f1b31d4b54c0ce5af
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-27 19:33:42 +0530

    refactor: formatted validations messages

--- intermediate_project/tickets/src/lib/validations.ts
+++ intermediate_project/tickets/src/lib/validations.ts
@@ -4,16 +4,16 @@ export const validateCreateTicket = [
     body('title')
         .trim()
         .isLength({ min: 3 })
-        .withMessage('Title must be at least 3 characters long'),
+        .withMessage('Title must be at least 3 characters long.'),
     body('price')
         .isFloat({ gt: 0 })
-        .withMessage('Price must be greater than 0')
+        .withMessage('Price must be greater than 0.')
 ];
 
 export const validParamId = [
     param('id')
         .isMongoId()
-        .withMessage('Invalid ticket id')
+        .withMessage('Invalid ticket id.')
 ];
 
 export const validateUpdateTicket = [
