3d63dedb4028b23b0b0979177f8654c19b38cab7
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-27 07:37:06 +0530

    feat: exported all things inside event in single file

--- nats_project/src/events/index.ts
+++ nats_project/src/events/index.ts
@@ -1,2 +1,6 @@
 export * from "./base-listener";
+export * from "./base-publisher";
+export * from "./subjects";
+export * from "./ticket-created-event";
 export * from "./ticket-created-listener";
+export * from "./ticket-created-publisher";
