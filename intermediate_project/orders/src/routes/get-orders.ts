68d3f93219784cfc36c6406ff9d87c491b6a3520
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-28 09:19:21 +0530

    feat: completed get orders

--- intermediate_project/orders/src/routes/get-orders.ts
+++ intermediate_project/orders/src/routes/get-orders.ts
@@ -1,11 +1,13 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { Order } from "../models";
 
 const router = Router();
 
 router.get("/",
     async (req: Request, res: Response, next: NextFunction) => {
         try {
-            const orders: any = [];
+            const { id } = req.user!;
+            const orders = await Order.find({ user: id }).populate('ticket');
             res.status(200).send({ orders });
         } catch (error: any) {
             next(error);
