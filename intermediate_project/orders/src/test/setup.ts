211d8487ee92dc49c4b4a956401aa5b62b7ba12c
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-28 09:19:37 +0530

    feat: updated test setup with some helper funcs

--- intermediate_project/orders/src/test/setup.ts
+++ intermediate_project/orders/src/test/setup.ts
@@ -7,12 +7,16 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
+import { Ticket, Order, OrderStatus } from "../models";
+
 jest.mock("../nats-wrapper.ts");
 
 declare global {
     namespace NodeJS {
         interface Global {
             authenticate(): string[];
+            createTicket(): Promise<any>;
+            createOrder(): Promise<any>;
         }
     }
 }
@@ -33,8 +37,8 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-(global as any).authenticate = () => {
-    const id = new mongoose.Types.ObjectId().toHexString();
+(global as any).authenticate = (providedId?: string) => {
+    const id = providedId || new mongoose.Types.ObjectId().toHexString();
     const email = "jhondoe@example.com";
     const token = jwt.sign({
         id,
@@ -45,4 +49,26 @@ afterAll(async () => {
     const base64 = Buffer.from(sessionJSON).toString("base64");
     const cookie = [`session=${base64}=;path=/;httponly`];
     return cookie;
+};
+
+(global as any).createTicket = async () => {
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 10,
+
+    });
+    await ticket.save();
+    return ticket;
+};
+
+(global as any).createOrder = async (createdTicket?: any) => {
+    const ticket = createdTicket || await (global as any).createTicket();
+    const order = Order.build({
+        ticket,
+        status: OrderStatus.Created,
+        expiresAt: new Date(),
+        user: new mongoose.Types.ObjectId().toHexString(),
+    });
+    await order.save();
+    return order;
 };
