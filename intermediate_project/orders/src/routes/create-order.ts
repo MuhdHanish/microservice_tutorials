7488919ca6ab6e664052cf7b3b097b8955184031
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-28 09:18:44 +0530

    feat: competed create order

--- intermediate_project/orders/src/routes/create-order.ts
+++ intermediate_project/orders/src/routes/create-order.ts
@@ -1,11 +1,15 @@
 import {
     CustomHTTPError,
+    OrderStatus,
     validationHandler,
 } from "@hanishdev-ticketing/common";
+import { Order, Ticket } from "../models";
 import { validateCreateOrder } from "../lib";
 import { NextFunction, Request, Response, Router } from "express";
 
 const router = Router();
+
+const  EXPERRARION_WINDOW_SECONDS = 15 * 60;
 
 router.post("/",
     validationHandler(validateCreateOrder),
@@ -15,8 +19,23 @@ router.post("/",
             if (!ticket) {
                 throw new CustomHTTPError("Ticket is required.", 400);
             }
+            const existingTicket = await Ticket.findById(ticket);
+            if (!existingTicket) {
+                throw new CustomHTTPError("Ticket not found.", 404);
+            }
+            const isReserved = await existingTicket.isReserved();
+            if (isReserved) {
+                throw new CustomHTTPError("Ticket is already reserved.", 400);
+            }
             const { id } = req.user!;
-            const order = {} as any
+            const expiration = new Date();
+            expiration.setSeconds(expiration.getSeconds() + EXPERRARION_WINDOW_SECONDS);
+            const order = Order.build({
+                user: id,
+                status: OrderStatus.Created,
+                expiresAt: expiration,
+                ticket
+            })
             await order.save();
             res.status(201).send({ order });
         } catch (error: any) {
