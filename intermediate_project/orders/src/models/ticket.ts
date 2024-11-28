15adbf032dd9b9020248036b02aa50889db44b63
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-28 09:18:06 +0530

    feat: created ticket model

---
+++ intermediate_project/orders/src/models/ticket.ts
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+import { Order, OrderStatus } from "./order";
+
+interface ITicketAttrs {
+    title: string;
+    price: number;
+}
+
+export interface ITicket extends mongoose.Document {
+    title: string;
+    price: number;
+    isReserved: () => Promise<boolean>;
+}
+
+interface ITicketModel extends mongoose.Model<ITicket> {
+    build(attrs: ITicketAttrs): ITicket;
+}
+
+const ticketSchema = new mongoose.Schema<ITicket, ITicketModel>(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        }
+    },
+    {
+        timestamps: true,
+        toJSON: {
+            transform(_, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.__v;
+            }
+        }
+    }
+);
+
+
+ticketSchema.methods.isReserved = async function () {
+    return !!await Order.findOne({
+        ticket: this,
+        status: {
+            $in: [
+                OrderStatus.Created,
+                OrderStatus.AwaitingPayment,
+                OrderStatus.Complete
+            ]
+        }
+    });
+};
+
+ticketSchema.statics.build = (attrs: ITicketAttrs) => {
+    return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<ITicket, ITicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
