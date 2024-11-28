cc05633197b6be219c132edf8ce694b8634be5b5
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-28 09:18:57 +0530

    feat: completed delete order

--- intermediate_project/orders/src/routes/delete-order.ts
+++ intermediate_project/orders/src/routes/delete-order.ts
@@ -4,6 +4,7 @@ import {
 } from "@hanishdev-ticketing/common";
 import { validParamId } from "../lib";
 import { Request, Response, NextFunction, Router } from "express";
+import { Order } from "../models";
 
 const router = Router();
 
@@ -15,7 +16,12 @@ router.delete("/:id",
             if (!id) {
                 throw new CustomHTTPError("ID is required.", 400);
             }
-            res.status(204);
+            const user = req.user!;
+            const order = await Order.findOneAndDelete({ _id: id, user: user.id });
+            if (!order) {
+                throw new CustomHTTPError("Order not found with provided ID and user.", 404);
+            }
+            res.sendStatus(204);
         } catch (error) {
             next(error);
         }
