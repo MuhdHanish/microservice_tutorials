214268dba96895b9c3475373040ffc96943212e1
MuhdHanish <muhammedhanish11@gmail.com>
2024-11-30 09:37:50 +0530

    feat: taking version from ticket updated listener and storing indb

--- intermediate_project/orders/src/events/listeners/ticket-updated-listener.ts
+++ intermediate_project/orders/src/events/listeners/ticket-updated-listener.ts
@@ -8,14 +8,15 @@ export class TicketUpdatedListener extends Listener<TickUpdatedEvent> {
     queueGroupName = TICKETS_LISTENER_QUEUE_GROUP;
 
     async onMessage(data: TickUpdatedEvent["data"], msg: Message): Promise<void> {
-        const { id } = data;
-        const ticket = await Ticket.findById(id);
+        const { id, title, price, version } = data;
+        const ticket = await Ticket.findByIdAndVersion({ id, version });
         if (!ticket) {
             throw new Error("Ticket not found");
         }
         ticket.set({
-            title: data.title,
-            price: data.price
+            title,
+            price,
+            version
         });
         await ticket.save();
         
