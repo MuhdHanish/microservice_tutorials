039a0ea1fbf82bab5f3708976bbf6035c1f7494f
MuhdHanish <muhammedhanish11@gmail.com>
2024-12-01 14:11:50 +0530

    feat: added delay for the expiratinQueue

--- intermediate_project/expiration/src/events/listeners/order-created-listener.ts
+++ intermediate_project/expiration/src/events/listeners/order-created-listener.ts
@@ -1,15 +1,17 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@hanishdev-ticketing/common";
 import { EXPIRATION_LISTENER_QUEUE_GROUP } from "../queue-groups";
-import { expirationQueue } from "../queues";
+import { expirationQueue } from "../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
     readonly subject = Subjects.OrderCreated;
     queueGroupName = EXPIRATION_LISTENER_QUEUE_GROUP;
 
     async onMessage(data: OrderCreatedEvent['data'], msg: any) {
+        const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+        console.log(`Waiting ${delay}ms to process the job...`);
         await expirationQueue.add({
             order: data.id
-        });
+        }, { delay });
         msg.ack();
     }
 }
