84001e0f303297368f33d622f02c50a2e7b3704b
MuhdHanish <muhammedhanish11@gmail.com>
2024-12-15 15:43:10 +0530

    feat: updated navbar button style

--- intermediate_project/client/components/navbar.jsx
+++ intermediate_project/client/components/navbar.jsx
@@ -11,34 +11,34 @@ export const Navbar = ({ user }) => {
             window.location.href = "/";
         });
     }
-  return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-          <div className="container-fluid">
-              <Link href="/" className="navbar-brand">
-                  Ticketing
-              </Link>
-              <div className="d-flex">
-                  {user ? (
-                      <button
-                          disabled={loading}
-                          type="button"
-                          className="text-danger outline-none border-0 bg-transparent" 
-                          onClick={onSignOut}
-                      >
-                          Sign Out
-                      </button>
-                  ) : (
-                      <>
-                          <Link href="/auth/signin" className="text-primary text-decoration-none me-3">
-                              Sign In
-                          </Link>
-                              <Link href="/auth/signup" className="text-primary text-decoration-none">
-                              Sign Up
-                          </Link>
-                      </>
-                  )}
-              </div>
-          </div>
-      </nav>
-  )
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div className="container-fluid">
+                <Link href="/" className="navbar-brand">
+                    Ticketing
+                </Link>
+                <div className="d-flex">
+                    {user ? (
+                        <button
+                            disabled={loading}
+                            type="button"
+                            className="text-danger outline-none border-0 bg-transparent"
+                            onClick={onSignOut}
+                        >
+                            Sign Out
+                        </button>
+                    ) : (
+                        <>
+                            <Link href="/auth/signin" className="text-primary text-decoration-none me-3">
+                                Sign In
+                            </Link>
+                            <Link href="/auth/signup" className="text-primary text-decoration-none">
+                                Sign Up
+                            </Link>
+                        </>
+                    )}
+                </div>
+            </div>
+        </nav>
+    )
 }
