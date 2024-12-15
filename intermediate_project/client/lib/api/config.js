a90f4b6d8232f1fb2ebb8f7de6bdd629255c3b41
MuhdHanish <muhammedhanish11@gmail.com>
2024-12-15 15:42:27 +0530

    refactor: updated api config

--- intermediate_project/client/lib/api/config.js
+++ intermediate_project/client/lib/api/config.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 
+let serverAxiosInstance = null;
+let clientAxiosInstance = null;
+
 export const axiosInstace = ({ req }) => {
     const isServer = typeof window === "undefined";
-    return isServer
-        ? axios.create({
-            baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
-            headers: req.headers
-        })
-        : axios.create({
-            baseURL: "/"
+    if (isServer) {
+        if (!serverAxiosInstance) {
+            serverAxiosInstance = axios.create({
+                baseURL: "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+                headers: req?.headers, 
+            });
+        }
+        return serverAxiosInstance;
+    }
+    if (!clientAxiosInstance) {
+        clientAxiosInstance = axios.create({
+            baseURL: "/",
         });
+    }
+    return clientAxiosInstance;
 }
