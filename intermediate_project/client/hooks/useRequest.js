98d95ed04c7532e4561942169291305ba761a5e5
MuhdHanish <muhammedhanish11@gmail.com>
2024-12-16 09:41:11 +0530

    feat: added data passing in onSuccess function

--- intermediate_project/client/hooks/useRequest.js
+++ intermediate_project/client/hooks/useRequest.js
@@ -6,19 +6,19 @@ export const useRequest = (url, method = "get", body = null, config = {}) => {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const makeRequest = async (onSuccess = () => { }, overrideUrl = null, overrideMethod = null, overrideBody = null, overrideConfig = {}) => {
+    const makeRequest = async (onSuccess = (data = null) => { }, overrideUrl = null, overrideMethod = null, overrideBody = null, overrideConfig = {}) => {
         setError(null);
         setLoading(true);
         try {
-            const response = await axios({
+            const { data } = await axios({
                 url: overrideUrl || url,
                 method: overrideMethod || method,
                 data: overrideBody || body,
                 ...config,
                 ...overrideConfig,
             });
-            setData(response?.data);
-            onSuccess && onSuccess();
+            setData(data);
+            onSuccess && onSuccess(data);
         } catch (error) {
             setError(error.response?.data?.message || "An error occurred");
         } finally {
